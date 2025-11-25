c324333e7b4e29984696d62ee694a14480710a24
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-11-25 15:55:13 -0600

    add more stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
+
+
+
+
+
 set -euo pipefail
 IFS=$'\n\t'
 WORKDIR=$(pwd)
+
+
+
+startqemu() {
+	cd $WORKDIR
+	dd if=/dev/zero of=rootfs.img bs=1M count=256
+	mkfs.ext4 rootfs.img
+	sudo mount rootfs.img /mnt
+	sudo cp -a plasmatail_build/* /mnt/
+	sudo umount /mnt
+	qemu-system-x86_64 \
+	  -kernel plasmatail_build/boot/vmlinuz \
+	  -drive file=rootfs.img,format=raw \
+	  -append "root=/dev/sda rw console=ttyS0 init=/init" \
+
+}
+
+
+
+
+
+
+
+
 echo "Tails1154 Linux Builder"
 echo "Removing existing work dir"
 rm -rf plasmatail_build
@@ -17,9 +46,9 @@ cp arch/x86/boot/bzImage ../plasmatail_build/boot/vmlinuz
 cd $WORKDIR
 echo "Copying busybox"
 cp busybox/busybox plasmatail_build/bin/
-cp busybox/busybox plasmatail_build/bin/sh
-cp busybox/busybox plasmatail_build/bin/bash
 echo "Installing busybox"
 plasmatail_build/bin/busybox --install -s plasmatail_build/bin
 echo "Copying tailsinit shell init thing"
 cp tailsinit/init plasmatail_build/init
+echo "Starting qemu"
+startqemu
